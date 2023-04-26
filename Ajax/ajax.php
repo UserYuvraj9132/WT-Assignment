4ec3b17cad6174619abc5d12c151f98ebb79385e
Yuvraj Bargaje <131593205+UserYuvraj9132@users.noreply.github.com>
2023-04-25 23:54:14 -0700

    Update ajax.php

--- Ajax/ajax.php
+++ Ajax/ajax.php
@@ -2,7 +2,7 @@
 
 $db_hostname ="localhost";
 $db_username = "root";
-$db_password="shree@2002";
+$db_password="Yuvraj@702";
 $db_name ="database1";
 
 $conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
